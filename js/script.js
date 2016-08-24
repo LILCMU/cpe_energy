d4385be5681115d91e4e7c9f87ffbdeb1efda3ff
universestu <universestu@gmail.com>
2016-08-25 00:49:29 +0700

    Added jquery 2.1.4 and new config file

--- js/script.js
+++ js/script.js
@@ -13,12 +13,13 @@ var list = []
 var max,timemax;
 var saveData = [];
 var lastUpdate ;
+var firstvalue = 0 ;
 var numAir = [];
 var nameOfSeries ;
 var max = 0;
 var maxNo = 0;
 var lastEntry = [];
-var roomData = [{"name":"401","list":[{"no":30},{"no":31},{"no":28}],"sum_unit_used":0},{"name":"402","list":[{"no":1},{"no":3},{"no":5}],"sum_unit_used":0},{"name":"403","list":[{"no":4}],"sum_unit_used":0},{"name":"404","list":[{"no":2}],"sum_unit_used":0},{"name":"405","list":[{"no":6}],"sum_unit_used":0},{"name":"406","list":[{"no":7},{"no":9}],"sum_unit_used":0},{"name":"407","list":[{"no":11}],"sum_unit_used":0},{"name":"409","list":[{"no":8}],"sum_unit_used":0},{"name":"410","list":[{"no":10}],"sum_unit_used":0},{"name":"411(LD)","list":[{"no":13},{"no":15}],"sum_unit_used":0},{"name":"411","list":[{"no":12}],"sum_unit_used":0},{"name":"412(4th)","list":[{"no":20}],"sum_unit_used":0},{"name":"412(1)","list":[{"no":16}],"sum_unit_used":0},{"name":"412(2)","list":[{"no":14}],"sum_unit_used":0},{"name":"412(N)","list":[{"no":34}],"sum_unit_used":0},{"name":"413","list":[{"no":21},{"no":23}],"sum_unit_used":0},{"name":"413(Gra)","list":[{"no":19},{"no":24}],"sum_unit_used":0},{"name":"414","list":[{"no":26}],"sum_unit_used":0},{"name":"415(LIL)","list":[{"no":25},{"no":29}],"sum_unit_used":0},{"name":"415(3rd)","list":[{"no":22},{"no":29}],"sum_unit_used":0},{"name":"SIPA","list":[{"no":32}],"sum_unit_used":0},{"name":"422","list":[{"no":33}],"sum_unit_used":0}];
+var roomData = [];
 var room4rdFloor = [["401","28","30","31"],
 ["402","1","3","5"],
 ["403","4"],
@@ -132,19 +133,11 @@ tooltip: {
 	valueDecimals: 2,
 },
 
-// plotOptions: {
-// 	// series: {
-// 	//     compare: 'percent'
-// 	// }
-// 	enabled: true
-// },
 plotOptions: {
-//          	line: {
-//              	dataLabels: {
-//               	   enabled: false
-//              	},
-//              	enableMouseTracking: true
-//          	}
+	 series: {
+                connectNulls: true
+            }
+	,
 bar: {
 	dataLabels: {
 		enabled: true
@@ -248,12 +241,15 @@ tooltip: {
 // 	enabled: true
 // },
 plotOptions: {
+//				 series: {
+  //              connectNulls: true
+    //        }
 //          	line: {
 //              	dataLabels: {
 //               	   enabled: false
 //              	},
 //              	enableMouseTracking: true
-//          	}
+//	,
 bar: {
 	dataLabels: {
 		enabled: true
@@ -298,12 +294,13 @@ function parseDataLog(data){
 	var date = new Date(data.datetime);
 	var localdate = date-1*date.getTimezoneOffset()*60*1000;
 	data.datetime = localdate;
-	data.value    = Number(data.value);
+	data.value    = parseFloat((Number(data.value)).toFixed(2));
 	return data;
 }
 
 function fetchData(option){
 	var roomCounter = 0;
+	
 	$.each(roomData, function(room_index, room){
 		if (room.name!=params.roomID){return;}
 		var sensorCounter = 0;
@@ -318,12 +315,16 @@ function fetchData(option){
 			var fetch_url   = serverURL+channelID+'/field/'+field+'.json?'+$.param(option);
 			$.getJSON(fetch_url, function (data) {
 				list = [];
+				
 				for (var index in data.feeds){
 					var record = data.feeds[index]
 					parsedData = parseDataLog({ datetime:record.created_at,value:record["field"+field]});
-					list.push( [parsedData.datetime, parseFloat((parsedData.value).toFixed(2))] )
+					list.push( [parsedData.datetime, (record["field"+field] == null ? null:parsedData.value)] )
+					
 					lastUpdate = record.created_at
 				}
+				console.log(lastUpdate)
+				
 				if (field == 1) {
 					lastEntry.push([sensor.no,parsedData.value])
 					seriesOptions[sensor_index] =
@@ -344,13 +345,14 @@ function fetchData(option){
 				if (sensorCounter === room.list.length&&field == 1) {
 					roomCounter += 1;
 					createChart();
-					lastStat(seriesOptions)
+					
 					for (var i = lastEntry.length - 1; i >= 0; i--) {
 						if (lastEntry[i][1] > maxValue){
 							maxValue = lastEntry[i][1]
 							maxNo = lastEntry[i][0]
 						}
 					};
+					lastStat(seriesOptions)
 					document.getElementById("Max.value").innerHTML = maxValue.toFixed(2)+ " kWh";
 					document.getElementById("Max.no").innerHTML = "No. " + maxNo;
 				}
@@ -390,20 +392,55 @@ function calStat (datain) {
 	document.getElementById("peak.time").innerHTML = timemax;
 	document.getElementById("peak.no").innerHTML = sensor;
 }
+
 function lastStat (data) {
 	data = data[0].data;
 	sumValue = 0
 	order = 0
-	for(var index in lastEntry){
-		sumValue +=lastEntry[index][1]
-	}
 
-	lastUnitEntryDate = moment(data[data.length-1][0]-25200000).format('DD-MMM-YYYY HH:mm:ss')
-	lastUnitEntry = sumValue
+	 fetchDataOnePoint({results : 1, end:moment(start).format('YYYY-MM-DD HH:mm:ss'), type:'unit_start'});
+     fetchDataOnePoint({results : 1, end:moment(end).format('YYYY-MM-DD HH:mm:ss'), type:'unit_end'});
 
-	document.getElementById("Unit.value").innerHTML = lastUnitEntry.toFixed(2)+ " kWh";
+
+	lastUnitEntryDate = moment(end).format('DD-MMM-YYYY HH:mm:ss')
 	document.getElementById("Unit.time").innerHTML = lastUnitEntryDate;
 }
+
+function fetchDataOnePoint(option){
+    //console.log("Loading...");
+    
+    var roomCounter = 0;
+
+    $.each(roomData, function(room_index, room){
+    	if (room.name!=params.roomID){return;}
+        var sensorCounter = 0;
+
+        $.each(room.list, function(sensor_index, sensor){
+            option          = option || {results : 1};
+            var channelID   = sensor.channels;
+            var field       = option.field || 1;
+            var fetch_url   = serverURL+channelID+'/field/'+field+'.json?'+$.param(option);
+            room.sum_unit_used = 0;    
+
+            $.getJSON(fetch_url, function (data) {
+                sensor[option.type] = (data.feeds.length > 0 ) ? Number(data.feeds[0].field1) : 0;
+                if (option.type=='unit_end'){
+                    sensor.unit_used = (sensor.unit_end >= sensor.unit_start ) ? (sensor.unit_end - sensor.unit_start):0;
+                     room.sum_unit_used += sensor.unit_used;
+                    // roomData1.push(room.sum_unit_used);
+                  	var firstvalue = room.sum_unit_used
+                    document.getElementById("Unit.value").innerHTML = firstvalue.toFixed(2) + " kWh";
+                }
+
+                }).complete(function() { 
+                });
+            });
+        });
+    
+
+    return;
+}
+
 function handleLoaded(series){
 // push data every 5 seconds
 setInterval(function() {
@@ -435,6 +472,20 @@ setInterval(function() {
 }, 5000);
 }
 function getTimedate() {
+	 var json = (function () {
+        var json = null;
+                    $.ajax({
+                        'async': false,
+                        
+                        'url': "js/config.json",
+                        'dataType': "json",
+                        'success': function (data) {
+                            json = data.room;
+                        }
+                    });
+                return json;
+    })(); 
+    roomData = json;
 	start = getUrlParameter('start');
 	end = getUrlParameter('end');
 	start = (typeof start == 'string') ? moment(decodeURI(start)) : moment().subtract(30, 'days');
@@ -445,20 +496,21 @@ function getTimedate() {
 
 }
 function cb(start, end) {
-	$('#reportrange span').html(start.format('MMMM D, YYYY') + ' to ' + end.format('MMMM D, YYYY'));
-//console.log(moment(start).format('YYYY-MM-DD HH:mm:ss'))
-//console.log(moment(end).format('YYYY-MM-DD HH:mm:ss'))
+			$('#reportrange span').html(start.format('MMMM D, YYYY') + ' to ' + end.format('MMMM D, YYYY'));
+		//console.log(moment(start).format('YYYY-MM-DD HH:mm:ss'))
+		//console.log(moment(end).format('YYYY-MM-DD HH:mm:ss'))
 
-channelKey =0
-params = getUrlParameter();
-validateParams();
-fetchData({field: 1 ,start : moment(start).format('YYYY-MM-DD HH:mm:ss'),end : moment(end).format('YYYY-MM-DD HH:mm:ss')});
-fetchData({field: 3 ,start : moment(start).format('YYYY-MM-DD HH:mm:ss'),end : moment(end).format('YYYY-MM-DD HH:mm:ss')});
-document.getElementById("lastTimeUpdate").innerHTML = moment(lastUpdate).format('DD MMMM YYYY HH:mm:ss');
-document.getElementById("RoomIdHtml").innerHTML = "Room  : "+  params.roomID;
-document.title = params.roomID   + " - Power Usage ";
-return;
-//Fetch channel information
+		channelKey =0
+		params = getUrlParameter();
+		validateParams();
+		console.log( moment(start).format('YYYY-MM-DD HH:mm:ss'))
+		fetchData({field: 1 ,start : moment(start).format('YYYY-MM-DD HH:mm:ss'),end : moment(end).format('YYYY-MM-DD HH:mm:ss'),median: 10});
+		fetchData({field: 3 ,start : moment(start).format('YYYY-MM-DD HH:mm:ss'),end : moment(end).format('YYYY-MM-DD HH:mm:ss'),median: 10});
+		document.getElementById("lastTimeUpdate").innerHTML = moment(lastUpdate).format('DD MMMM YYYY HH:mm:ss');
+		document.getElementById("RoomIdHtml").innerHTML = "Room  : "+  params.roomID;
+		document.title = params.roomID   + " - Power Usage ";
+		return;
+		//Fetch channel information
 }
 function initDatePicker(){
 	$('#reportrange').daterangepicker({
